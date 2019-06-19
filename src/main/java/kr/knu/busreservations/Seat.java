369ea2257fd75040031bd1417ad8ffec35883e28
Songeun Yoo <33443281+SongeunYoo@users.noreply.github.com>
2019-06-19 15:42:08 +0900

    add constructor

--- src/main/java/kr/knu/busreservations/Seat.java
+++ src/main/java/kr/knu/busreservations/Seat.java
@@ -6,4 +6,16 @@ public class Seat {
     int ticketId;
     int userId;
 
+    Seat(int seatId, boolean occupied) {
+        this.seatId = seatId;
+        this.occupied = occupied;
+    }
+
+    Seat(int seatId, boolean occupied, int ticketId, int userId) {
+        this.seatId = seatId;
+        this.occupied = occupied;
+        this.ticketId = ticketId;
+        this.userId = userId;
+    }
+
 }
--- src/main/java/kr/knu/busreservations/Terminal.java
+++ src/main/java/kr/knu/busreservations/Terminal.java
@@ -3,4 +3,9 @@ package kr.knu.busreservations;
 public class Terminal {
     int id;
     String name;
+
+    Terminal(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
 }
